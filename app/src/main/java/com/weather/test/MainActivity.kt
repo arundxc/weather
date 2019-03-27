272f1eaf689603c3d2554e75db0d386e9c97bea9
arundxc <dxcarun@gmail.com>
2019-03-27 06:57:15 +0530

    databinding added`

--- app/src/main/java/com/weather/test/MainActivity.kt
+++ app/src/main/java/com/weather/test/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.weather.test
 
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.weather.test.databinding.ActivityMainBinding
+import com.weather.test.pojo.WeatherModule
 
 class MainActivity : AppCompatActivity() {
 
+
+    //lateinit var binding : ActivityMainBinding
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        //var weatherModule = WeatherModule()
+        //binding.weather=(weatherModule)
+
+
     }
 }
