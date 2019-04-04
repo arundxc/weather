bb06ca79c47efcc189e4f8d3b694814ab7d1aec9
arundxc <dxcarun@gmail.com>
2019-04-04 05:55:35 +0530

    temp file is added

---
+++ app/src/main/java/com/weather/test/pojo/Temp.java
@@ -0,0 +1,13 @@
+package com.weather.test.pojo;
+
+import android.database.Observable;
+import android.databinding.ObservableField;
+
+/**
+ * Created by Arun on 03/04/19.
+ */
+
+public class Temp {
+
+    public ObservableField<String> temp = new ObservableField<>("Arun");
+}
